35b379cac1a67457f0ea2f26b36ec5fd3a3119c1
capitancambio <capitan.cambio@gmail.com>
2014-01-16 18:59:26 +0000

    Improve patternprocessor

--- src/core/PatternProcessor.m
+++ src/core/PatternProcessor.m
@@ -1,29 +1,22 @@
 classdef PatternProcessor<handle
-
+        %Process the 
 	properties
 		next
 	end
 
 	methods 
-		function self=PatternProcessor()
-			self.next=0;
-		end
 		function [trainData,testData]=process(self,trainData,testData)
 			[trainData,testData]=self.doProcess(trainData,testData);
-			if isa(self.next,'double')==0
-				[trainData,testData]=self.next.process(trainData,testData);
+			if ~isempty(self.next)
+                                [trainData,testData]=self.next.process(trainData,testData);
 			end
-			[trainData,testData]=self.postProcess(trainData,testData);
+			%[trainData,testData]=self.postProcess(trainData,testData);
 							
 		end
 		function next=setNext(self,nextProcessor)
 			self.next=nextProcessor;
 			next=self.next;
 		end
-		function [trainData,testData]= postProcess(self,trainData,testData)
-			
-		end
-
 	end	
 
 	methods(Abstract)
